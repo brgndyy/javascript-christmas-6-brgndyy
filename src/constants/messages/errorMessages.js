94271d05f3a104ff33e1a72eee2a57451c77908a
brgndy <brgndy96@gmail.com>
2023-11-11 13:20:14 +0900

    feat(constants): 주문 관련 에러메세지, 입력메세지 추가

--- src/constants/messages/errorMessages.js
+++ src/constants/messages/errorMessages.js
@@ -2,6 +2,7 @@ import deepFreeze from '../../utils/deepFreeze.js';
 
 const ERROR_MESSAGES = deepFreeze({
   not_correct_date: '유효하지 않은 날짜입니다. 다시 입력해 주세요.',
+  not_correct_order: '유효하지 않은 주문입니다. 다시 입력해 주세요.',
 });
 
 export default ERROR_MESSAGES;
--- src/constants/messages/orderMessages.js
+++ src/constants/messages/orderMessages.js
@@ -3,6 +3,8 @@ import deepFreeze from '../../utils/deepFreeze.js';
 const ORDER_MESSAGES = deepFreeze({
   start_message: `안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.`,
   select_date: '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n',
+  select_menu_quantity:
+    '주문하실 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n',
 });
 
 export default ORDER_MESSAGES;
