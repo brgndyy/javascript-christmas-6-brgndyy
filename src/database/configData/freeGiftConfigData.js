5c934d66af19572158f79ab5635d6c2e187f6728
brgndy <brgndy96@gmail.com>
2023-11-12 15:27:13 +0900

    feat(data): 증정메뉴 관련 데이터 생성 및 전체 데이터 형식 프로퍼티 추가

---
+++ src/database/configData/freeGiftConfigData.js
@@ -0,0 +1,7 @@
+import deepFreeze from '../../utils/deepFreeze.js';
+
+const FREE_GIFT_CONDITION_CONFIG_DATA = deepFreeze({
+  price: 120000,
+});
+
+export default FREE_GIFT_CONDITION_CONFIG_DATA;
---
+++ src/database/menus/freeGiftMenu.js
@@ -0,0 +1,5 @@
+import ALL_MENU_DATA from './allMenu.js';
+
+const FREE_GIFT_MENU = ALL_MENU_DATA.find((item) => item.isFreeGift);
+
+export default FREE_GIFT_MENU;
