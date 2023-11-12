106a6e36bd68b27addc18b286e747d6ad421189c
brgndy <brgndy96@gmail.com>
2023-11-12 18:15:03 +0900

    refactor(test) : 주문 규칙 유효성 검증 테스트 코드 수정

--- __tests__/OrderValidator/OrderRulesValidatorTest.js
+++ __tests__/OrderValidator/OrderRulesValidatorTest.js
@@ -25,22 +25,22 @@ describe('OrderRulesValidator', () => {
       [{ menu: '제로콜라', quantity: 3 }],
       [{ menu: '바비큐립', quantity: -1 }],
     ];
-    expect(() => orderRulesValidator.validateOrderRules(orderList)).toThrow(OrderError);
+    expect(() => orderRulesValidator.validateOrderRules(orderList)).toThrow(new OrderError());
   });
 
   test('총 주문 수량 초과시 예외처리', () => {
-    const orderList = [{ menu: '메뉴1', quantity: ORDER_CONFIG_DATA.max_quantity + 1 }];
-    expect(() => orderRulesValidator.validateOrderRules(orderList)).toThrow(OrderError);
+    const orderList = [{ menu: '양송이수프', quantity: ORDER_CONFIG_DATA.max_quantity + 1 }];
+    expect(() => orderRulesValidator.validateOrderRules(orderList)).toThrow(new OrderError());
   });
 
   test('주문 수량이 음수일때 예외처리', () => {
     const orderList = [{ menu: '저가격 메뉴', quantity: -2 }];
-    expect(() => orderRulesValidator.validateOrderRules(orderList)).toThrow(OrderError);
+    expect(() => orderRulesValidator.validateOrderRules(orderList)).toThrow(new OrderError());
   });
 
   test('음료만 시켰을때 예외 처리', () => {
     const orderList = [{ menu: '제로콜라', quantity: 3 }];
-    expect(() => orderRulesValidator.validateOrderRules(orderList)).toThrow(OrderError);
+    expect(() => orderRulesValidator.validateOrderRules(orderList)).toThrow(new OrderError());
   });
 
   test('유효한 주문', () => {
