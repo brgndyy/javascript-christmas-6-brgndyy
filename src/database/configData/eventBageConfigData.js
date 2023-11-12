a3286ade8de67b9e997ef548ca7f85e7f09eb2d6
brgndy <brgndy96@gmail.com>
2023-11-12 18:56:41 +0900

    feat(data): 이벤트 뱃지 관련 데이터 객체 생성

---
+++ src/database/configData/eventBageConfigData.js
@@ -0,0 +1,9 @@
+import deepFreeze from '../../utils/deepFreeze.js';
+
+const EVENT_BADGE_CONFIG_DATA = deepFreeze([
+  { badge: '산타', minAmount: 20000 },
+  { badge: '트리', minAmount: 10000 },
+  { badge: '별', minAmount: 5000 },
+]);
+
+export default EVENT_BADGE_CONFIG_DATA;
