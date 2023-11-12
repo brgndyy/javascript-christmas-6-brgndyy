0469fb6d4157c02faf9cfe8e9b0d37572ae514e9
brgndy <brgndy96@gmail.com>
2023-11-12 17:08:08 +0900

    refactor(test): 증정 메뉴 import 방식 변경

--- __tests__/FreeGiftService/FreeGiftServiceTest.js
+++ __tests__/FreeGiftService/FreeGiftServiceTest.js
@@ -1,6 +1,6 @@
 import FreeGiftService from '../../src/service/FreeGiftService.js';
 import FREE_GIFT_CONDITION_CONFIG_DATA from '../../src/database/configData/freeGiftConfigData.js';
-import FREE_GIFT_MENU from '../../src/database/menus/freeGiftMenu.js';
+import { FREE_GIFT_MENU } from '../../src/database/menus/freeGiftMenu.js';
 
 describe('FreeGiftService 클래스에 대한 테스트', () => {
   test('총 주문 금액이 증정 이벤트 조건을 충족할 때 증정 선물을 반환한다', () => {
