c348faac1852c6d421901f2ac94648f1f500794b
brgndy <brgndy96@gmail.com>
2023-11-12 15:50:49 +0900

    refactor(gift): 확장성을 위하여 find 가 아닌 filter 메서드로 변경

--- src/database/menus/freeGiftMenu.js
+++ src/database/menus/freeGiftMenu.js
@@ -1,5 +1,5 @@
 import ALL_MENU_DATA from './allMenu.js';
 
-const FREE_GIFT_MENU = ALL_MENU_DATA.find((item) => item.isFreeGift);
+const FREE_GIFT_MENU = ALL_MENU_DATA.filter((item) => item.isFreeGift);
 
 export default FREE_GIFT_MENU;
