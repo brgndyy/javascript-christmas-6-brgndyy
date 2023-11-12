79a378a4920634c65984eb20a4e685da5ad73d0b
brgndy <brgndy96@gmail.com>
2023-11-12 15:29:22 +0900

    feat(freeGift):  증정 메뉴 조건을 담당할 서비스 클래스 생성

---
+++ src/service/FreeGiftService.js
@@ -0,0 +1,15 @@
+import FREE_GIFT_CONDITION_CONFIG_DATA from '../database/configData/freeGiftConfigData.js';
+
+class FreeGiftService {
+  /**
+   * 무료 증정 혜택에 대한 검증을 수행합니다.
+   * @param { number } totalPrice
+   * @returns { boolean}
+   */
+
+  static isEligibleForFreeGift(totalPrice) {
+    return totalPrice > FREE_GIFT_CONDITION_CONFIG_DATA.price;
+  }
+}
+
+export default FreeGiftService;
