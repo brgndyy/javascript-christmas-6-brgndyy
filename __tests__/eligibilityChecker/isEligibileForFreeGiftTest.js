e438096542d40a58e8c2b639be35bb957a291782
brgndy <brgndy96@gmail.com>
2023-11-13 13:37:30 +0900

    refactor(eligible): 이벤트 배지 및 증정 이벤트 객체로 변경

--- __tests__/eligibilityChecker/isEligibileForFreeGiftTest.js
+++ __tests__/eligibilityChecker/isEligibileForFreeGiftTest.js
@@ -1,17 +1,17 @@
-import FreeGiftService from '../../src/service/FreeGiftService.js';
+import eligibilityChecker from '../../src/service/eligibilityChecker.js';
 import FREE_GIFT_CONFIG_DATA from '../../src/database/configData/freeGiftConfigData.js';
 
-describe('FreeGiftService 클래스에 대한 테스트', () => {
+describe('eligibilityChecker 객체의 선물 증정 이벤트에 대한 조건 충족 판단 함수 테스트', () => {
   test('총 주문 금액이 증정 이벤트 조건을 충족할 때 증정 선물을 반환한다', () => {
     const totalPrice = FREE_GIFT_CONFIG_DATA.total_price + 1;
-    const result = FreeGiftService.isEligibleForFreeGift(totalPrice);
+    const result = eligibilityChecker.isEligibleForFreeGift(totalPrice);
 
     expect(result).toEqual(FREE_GIFT_CONFIG_DATA.menu);
   });
 
   test('총 주문 금액이 증정 이벤트 조건을 충족하지 않을 때 false를 반환한다', () => {
     const totalPrice = FREE_GIFT_CONFIG_DATA.total_price - 1;
-    const result = FreeGiftService.isEligibleForFreeGift(totalPrice);
+    const result = eligibilityChecker.isEligibleForFreeGift(totalPrice);
 
     expect(result).toBeFalsy();
   });
