b206a4e1669e1932cf7dc1d3fd217c2478a8c100
brgndy <brgndy96@gmail.com>
2023-11-13 09:33:47 +0900

    refactor(data): 확장성을 고려해 이벤트 데이터에 slug 프로퍼티 추가

--- src/database/configData/eventConfigData.js
+++ src/database/configData/eventConfigData.js
@@ -1,34 +1,42 @@
 import deepFreeze from '../../utils/deepFreeze.js';
 import { TOTAL_FREE_GIFT_PRICE } from '../menus/freeGiftMenu.js';
 
-export const WEEK_DAY_EVENT_CONFIG_DATA = deepFreeze({
-  title: '평일 할인',
-  sale_price: 2023,
-  sale_category: 'dessert',
-});
+const EVENT_CONFIG_DATA = deepFreeze([
+  {
+    id: 1,
+    title: '평일 할인',
+    slug: 'weekday',
+    sale_price: 2023,
+    sale_category: 'dessert',
+  },
+  {
+    id: 2,
+    title: '주말 할인',
+    slug: 'weekend',
+    sale_price: 2023,
+    sale_category: 'main',
+  },
+  {
+    id: 3,
+    title: '특별 할인',
+    slug: 'special',
+    sale_price: 1000,
+  },
+  {
+    id: 4,
+    title: '증정 이벤트',
+    slug: 'free_gift',
+    sale_price: TOTAL_FREE_GIFT_PRICE,
+  },
+  {
+    id: 5,
+    title: '크리스마스 디데이 할인',
+    slug: 'target_event',
+    sale_basic_price: 1000,
+    sale_range: 100,
+    sale_start_day: 1,
+    sale_end_day: 25,
+  },
+]);
 
-export const WEEKEND_EVENT_CONFIG_DATA = deepFreeze({
-  title: '주말 할인',
-  sale_price: 2023,
-  sale_category: 'main',
-});
-
-export const SPECIAL_EVENT_CONFIG_DATA = deepFreeze({
-  title: '특별 할인',
-  sale_price: 1000,
-});
-
-export const FREE_GIFT_EVENT_CONFIG_DATA = deepFreeze({
-  title: '증정 이벤트',
-  sale_price: TOTAL_FREE_GIFT_PRICE,
-});
-
-export const TARGET_EVENT_CONFIG_DATA = deepFreeze({
-  title: '크리스마스 디데이 할인',
-  sale_basic_price: 1000,
-  sale_range: 100,
-  sale_start_day: 1,
-  sale_end_day: 25,
-});
-
-export const NONE_SALE_PRICE = deepFreeze(0);
+export default EVENT_CONFIG_DATA;
