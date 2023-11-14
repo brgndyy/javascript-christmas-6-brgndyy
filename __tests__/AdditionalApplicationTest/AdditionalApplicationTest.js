fa4c62e9a3888c84f79ffb6ae56af96aa0e784c7
brgndy <brgndy96@gmail.com>
2023-11-14 15:19:02 +0900

    rename(test): 작성한 테스트코드 내부 폴더로 이동

--- __tests__/AdditionalApplicationTest/AdditionalApplicationTest.js
+++ __tests__/AdditionalApplicationTest/AdditionalApplicationTest.js
@@ -1,9 +1,9 @@
-import App from '../src/App.js';
-import mockQuestions from '../src/test/testUtils/mockQuestions.js';
-import getLogSpy from '../src/test/testUtils/getLogSpy.js';
-import getOutput from '../src/test/testUtils/getOutput.js';
-import expectLogContains from '../src/test/testUtils/expectedLogContains.js';
-import expectedTestOutput from '../src/test/data/expectedTestOutput.js';
+import App from '../../src/App.js';
+import mockQuestions from '../../src/test/testUtils/mockQuestions.js';
+import getLogSpy from '../../src/test/testUtils/getLogSpy.js';
+import getOutput from '../../src/test/testUtils/getOutput.js';
+import expectLogContains from '../../src/test/testUtils/expectedLogContains.js';
+import expectedTestOutput from '../../src/test/data/expectedTestOutput.js';
 
 describe('추가적인 총체적인 어플리케이션 테스트', () => {
   test.each(expectedTestOutput)(
